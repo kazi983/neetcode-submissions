8bb9956f0defa4594af390a2e0bc5421f5679a83
kazi983 <kaoru.kajihara.mail@gmail.com>
2026-08-21 22:38:42 -0700

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,8 @@
+select 
+    u.name, 
+    COALESCE(sum(r.distance), 0) as travelled_distance
+from users u
+left join rides r
+    on r.user_id = u.id
+group by u.name
+order by travelled_distance desc;
