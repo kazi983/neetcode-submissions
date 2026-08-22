37bfb0f3938b98faebd6e671c313bdb7a4df1f10
kazi983 <kaoru.kajihara.mail@gmail.com>
2026-08-21 22:52:06 -0700

    Add: sql-sales-person - submission-1

---
+++ SQL/sql-sales-person/submission-1.sql
@@ -0,0 +1,14 @@
+WITH sales_company as (
+    select distinct c.name as com_name, o.sales_id
+    from orders o
+    join company c on c.com_id = o.com_id
+)
+
+select name 
+from sales_person p
+where not exists (
+    select 1 from sales_company sc
+    where 
+        p.sales_id = sc.sales_id
+        and com_name = 'CRIMSON'
+)
